944e49c2a7b5958e46a177c09e0d66d3a4d7eee6
Alexandra Stepanova <ale.stepanova@icloud.com>
2023-07-01 19:16:35 +0300

    feat: virw configuration added

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+    },
+  },
   plugins: [react()],
 })
