c4e7efa03aa23ba27e30e384d2bf9b45e06456db
Alexandra Stepanova <ale.stepanova@icloud.com>
2023-07-01 15:03:11 +0300

    refactor: unneeded code removed

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,18 +5,11 @@ const preview: Preview = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       hideNoControlsWarning: true,
-    expanded: true,
-    exclude: /^data-/i,
+      expanded: true,
+      exclude: /^data-/i,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
-      },
-    },
-    docs: {
-      transformSource(source, storyId) {
-        // Fix for @ariaProperty decorator effect, which displays aria properties with data- prefix in source code
-        // Replaces all "data-" attributes with unprefixed values
-        return source.replace(/\sdata-/gm, " ");
       },
     },
   },
