6c93b731526bac1757f536d89ab8253c57493220
alexandra-stepanova <ale.stepanova@icloud.com>
2023-07-01 18:21:30 +0000

    Deploying to gh-pages from @ alexandra-stepanova/button-react-ds@cf2d2b7b0af9b8deea834c1fafc01d5ca53d8266 🚀

---
+++ assets/preview-948ca25b.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-025937ed.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-a319ca81.js"),["assets/DocsRenderer-EYKKDMVH-a319ca81.js","assets/iframe-025937ed.js","assets/index-8db94870.js","assets/_commonjsHelpers-042e6b4d.js","assets/react-18-f58e807b.js","assets/index-80ae7d84.js","assets/index-ce0ed022.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-948ca25b.js.map
