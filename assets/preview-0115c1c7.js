ceee035efd40e3c52eb7d24aef9af7b833dece53
alexandra-stepanova <ale.stepanova@icloud.com>
2023-07-02 13:51:52 +0000

    Deploying to gh-pages from @ alexandra-stepanova/button-react-ds@0c43ace3ae986e5fb6bdead469e8a701b13c3aa1 🚀

---
+++ assets/preview-0115c1c7.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-22058989.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-ff3f0d98.js"),["assets/DocsRenderer-EYKKDMVH-ff3f0d98.js","assets/iframe-22058989.js","assets/index-8db94870.js","assets/_commonjsHelpers-042e6b4d.js","assets/react-18-f58e807b.js","assets/index-80ae7d84.js","assets/index-f251db49.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-0115c1c7.js.map
--- assets/preview-739b5ddc.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{hideNoControlsWarning:!0,expanded:!0,exclude:/^data-/i,matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-739b5ddc.js.map
