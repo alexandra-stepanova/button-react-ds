a5d32222ba7267c5ad8075eac9e226b7dae8ee73
alexandra-stepanova <ale.stepanova@icloud.com>
2023-07-02 13:01:31 +0000

    Deploying to gh-pages from @ alexandra-stepanova/button-react-ds@31762594be44c54e1e17670c101c00c2e4e672fc 🚀

---
+++ assets/Button.stories-9dcf11f0.js
@@ -0,0 +1,29 @@
+import{B as y}from"./Button-cf79844f.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const z={title:"Example/Button",component:y,tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},a={args:{id:"4",primary:!0,label:"Button",ariaLabel:"primary button"}},r={args:{id:"3",label:"Button",ariaLabel:"secondary button"}},e={args:{id:"1",size:"large",label:"Button",ariaLabel:"button large"}},o={args:{id:"2",size:"small",label:"Button",ariaLabel:"button small"}};var t,s,n;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    id: '4',
+    primary: true,
+    label: 'Button',
+    ariaLabel: 'primary button'
+  }
+}`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var l,c,i;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    id: '3',
+    label: 'Button',
+    ariaLabel: 'secondary button'
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,u,d;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    id: '1',
+    size: 'large',
+    label: 'Button',
+    ariaLabel: 'button large'
+  }
+}`,...(d=(u=e.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var p,b,g;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    id: '2',
+    size: 'small',
+    label: 'Button',
+    ariaLabel: 'button small'
+  }
+}`,...(g=(b=o.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};const x=["Primary","Secondary","Large","Small"];export{e as Large,a as Primary,r as Secondary,o as Small,x as __namedExportsOrder,z as default};
+//# sourceMappingURL=Button.stories-9dcf11f0.js.map
---
+++ assets/preview-d865aba6.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-1883e1dd.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-65755337.js").then(e=>e.D),["assets/DocsRenderer-EYKKDMVH-65755337.js","assets/iframe-1883e1dd.js","assets/index-8db94870.js","assets/_commonjsHelpers-042e6b4d.js","assets/_getPrototype-63c5d83d.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-d865aba6.js.map
