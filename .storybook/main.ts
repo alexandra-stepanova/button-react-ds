8664a050d6c48e4782af26facc2d75ccd8b5203f
Alexandra Stepanova <ale.stepanova@icloud.com>
2023-07-26 14:06:48 +0300

    fix error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+const { loadConfigFromFile, mergeConfig } = require("vite");
+const path = require("path");
 
 const config: StorybookConfig = {
   stories: [
@@ -19,6 +21,19 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+
+  async viteFinal(config) {
+    const { config: libConfig } = await loadConfigFromFile(
+      path.resolve(__dirname, "../vite.config.ts")
+    );
+    if (process.env.NODE_ENV === "production") {
+      config.base = "button-react-ds"; // base URL for production
+    }
+    if (process.env.BASE_URL) {
+      config.base = process.env.BASE_URL;
+    }
+    return mergeConfig(config, { plugins: libConfig.plugins });
   },
 };
 
